10fdeffadacefe6b7b463b951432d6d33d6d16d1
jose-arce06 <a224200192@unison.mx>
2026-01-21 18:42:37 -0700

    Change directory due to errors

--- tictactoe/game_logic/board.py
+++ tictactoe/game_logic/board.py
@@ -3,10 +3,22 @@ Docstring for tictactoe.game_logic.board
 Author: jose arce
 """
 
-def display_board(dboard:dict): None:
+def display_board(dboard:dict)->None:
     """
     Docstring for display_board
     """
-print(f"{d[0]}|{d[1]}|{d[3]}")
+    d= dboard
+    print(f"{dboard[0]}|{dboard[1]}|{dboard[2]}")
+    print("-+-+-")
+    print(f"{dboard[3]}|{dboard[4]}|{dboard[5]}")
+    print("-+-+-")
+    print(f"{dboard[6]}|{dboard[7]}|{dboard[8]}")
 
 
+# Example usage
+if __name__ == "__main__":
+    board = {x:str(x) for x in range(9)}
+    display_board(board)
+    board[0] = 'X'
+    board[4] = 'O'
+    display_board(board)   
