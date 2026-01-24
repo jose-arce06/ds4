310ee42d5dbccb7c38a6470ee93a670203aa1962
jose-arce06 <a224200192@unison.mx>
2026-01-23 18:58:55 -0700

    we added a combo_list whit the possible solution for wining

--- tictactoe/game_logic/game_logic.py
+++ tictactoe/game_logic/game_logic.py
@@ -3,12 +3,25 @@
 """
 import board
 
+def check_winner(d:dict, combo_list:list)->bool:
+    """
+    Check if there is a winner
+    """
+    for combo in combo_list:
+        if d[combo[0]] == d[combo[1]] == d[combo[2]]:
+            return True
+    return False
 def game():
     """
     Here lives the main game logic
     """
     turn = 0
     dboard = {x:str(x) for x in range(9)}
+    combo_list = [
+        [0,1,2], [3,4,5], [6,7,8], # rows
+        [0,3,6], [1,4,7], [2,5,8], # columns
+        [0,4,8], [2,4,6]           # diagonals
+    ]
     x_players = 'X'
     o_players = 'O'
     current_player = x_players
