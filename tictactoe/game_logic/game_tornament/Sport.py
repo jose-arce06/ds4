9bada7c88b4e601626b118e32a01c752518eb112
jose-arce06 <a224200192@unison.mx>
2026-02-06 18:26:39 -0700

    Finished Sport class

--- tictactoe/game_logic/game_tornament/Sport.py
+++ tictactoe/game_logic/game_tornament/Sport.py
@@ -1,5 +1,21 @@
-Class Sport:
+class Sport:
     def __init__(self, name, num_players, league):
         self.name = name
         self.league = league
+        self.num_players = num_players
+    def __str__(self):
+        return f"{self.name}({self.league}) - {self.num_players} players"
+    def __repr__(self):
+        return f"Sport(name={self.name}, league={self.league}), players={self.num_players})" 
+    def to_json(self):
+        return {
+            "name": self.name,
+            "league": self.league,
+            "num_players": self.num_players
+        }
     
+if __name__ == "__main__":
+    sport = Sport("Basketball", 10, "NBA")
+    print(sport)
+    print(sport.to_json())
+    print(repr(sport))
