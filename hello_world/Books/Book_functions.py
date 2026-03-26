6e391306f5d578d3d7506c5d9788c97bc79724e5
jose-arce06 <a224200192@unison.mx>
2026-03-25 22:26:59 -0700

    Added book detail page

--- hello_world/Books/Book_functions.py
+++ hello_world/Books/Book_functions.py
@@ -1,32 +1,42 @@
+""" book_functions.py
+    Contains functions to work with Book objects
+"""
+
 from Book import Book
 from Book import load_books
 
 def get_genres(books:list[Book])->list[str]:
-    """Return a list of unique genres from the list of books"""
+    """Get all unique genres from a list of Book objects"""
     genres = set()
     for book in books:
         genres.add(book.genre)
     return sorted(list(genres))
 
-def create_author_dictionary(books:list[Book])->dict[str, list[Book]]:
-    """Return a dictionary with authors as keys and a list of their books as values"""
+def create_author_dictionary(books:list[Book])->dict[str,list[Book]]:
+    """Create a dictionary of authors and their books"""
     author_dict = {}
     for book in books:
         if book.author.lower() not in author_dict:
             author_dict[book.author.lower()] = []
         author_dict[book.author.lower()].append(book)
+        # Multiple names or authors
         author_names = book.author.lower().split(" ")
         if len(author_names) >= 2:
-            for name in author_names:
+            for name in author_names:   
                 if name not in author_dict:
                     author_dict[name] = []
                 author_dict[name].append(book)
     return author_dict
 
+def create_book_dictionary(book_list:list)->dict[str,Book]:
+    """Create a dictionary of books and their ids"""
+    book_dict = {}
+    for book in book_list:
+        book_dict[book.id] = book
+    return book_dict
+
 if __name__ == "__main__":
-    # Load books from csv file
     books = load_books("booklist2000.csv")
     print(get_genres(books))
     author_dict = create_author_dictionary(books)
     print(author_dict["sandra"][0])
-
--- hello_world/Books/app.py
+++ hello_world/Books/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request
-from Book import Book, load_books
-from Book_functions import create_author_dictionary
+from Book import Book, load_books 
+import Book_functions
+
 app = Flask(__name__)
 
 filename = 'booklist2000.csv'
 books = load_books(filename)
-author_dict = create_author_dictionary(books)
+author_dict = Book_functions.create_author_dictionary(books)
+book_dict = Book_functions.create_book_dictionary(books)
 
 @app.route('/')
 def index():
@@ -19,5 +21,13 @@ def search_by_author():
         return render_template('search_by_author.html', books_list=books_list)
     else:
         return render_template('search_by_author.html', books_list=books[:10])
+
+@app.route('/book/<book_id>')
+def book_detail(book_id):
+    book = book_dict.get(book_id)
+    #print(book)
+    return render_template('book_detail.html', book=book)
+
+        
 if __name__ == '__main__':
     app.run(debug=True)
