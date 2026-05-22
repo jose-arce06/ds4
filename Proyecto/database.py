6d90e0bbea9d46daafd0fb688c9a5024caacb6a5
jose-arce06 <a224200192@unison.mx>
2026-05-21 21:27:29 -0700

    ya descargo los psf faltan algunos erores en la pagina pero ahi va y el ocr no funciona

--- Proyecto/database.py
+++ Proyecto/database.py
@@ -9,7 +9,7 @@ def get_db():
 
 def init_db():
     with get_db() as conn:
-       
+        # Tabla para las URLs raíz ingresadas en Configuration
         conn.execute('''
             CREATE TABLE IF NOT EXISTS sources (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,7 +17,7 @@ def init_db():
                 status TEXT DEFAULT 'no escrapeada'
             )
         ''')
-      
+        # Tabla para guardar los documentos individuales indexados
         conn.execute('''
             CREATE TABLE IF NOT EXISTS documents (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -31,4 +31,5 @@ def init_db():
             )
         ''')
         conn.commit()
+
 init_db()
